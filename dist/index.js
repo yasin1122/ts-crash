82e0378b4d720141b369edb757b323c44f4f05ed
Yasin Cakal <whoisyasin@gmail.com>
2025-04-15 21:29:39 -0700

    review completed

--- dist/index.js
+++ dist/index.js
@@ -13,3 +13,62 @@ people = [
     [1, 'Yasin', 36],
     [2, 'Beyza', 25]
 ];
+// Union
+let userId;
+userId = '88';
+let move = 'up'; // only 4 options above allowed
+// Enums
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Right"] = 2] = "Right";
+    Direction[Direction["Down"] = 3] = "Down";
+    Direction[Direction["Left"] = 4] = "Left";
+})(Direction || (Direction = {}));
+const typeUser1 = {
+    id: 1,
+    name: 'Yasin'
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid; // alternate way
+// Functions
+function concat(str1, str2) {
+    // if there is no return the return type is void
+    return str1 + ' ' + str2;
+}
+const interfaceUser1 = {
+    id: 1,
+    name: 'Yasin'
+};
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered!`;
+    }
+}
+const yasin = new Person(1, 'Yasin');
+// Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3]);
+let strArray = getArray(['HTML', 'CSS', 'JS']);
+// Partial<T>: Makes all properties in T optional.
+// Required<T>: Makes all properties in T required.
+// Readonly<T>: Makes all properties in T read-only.
+// Pick<T, K>: Picks a set of properties K from T.
+// Omit<T, K>: Constructs a type by omitting keys K from T.
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,123 @@ let ids: number[] = [1, 2, 3, 4, 5]
 
 // Tuple
 let person: [number, string, boolean] = [1, 'Yasin', true]
+
 // Tuple Array
 let people: [number, string, number][]
 people = [
   [1, 'Yasin', 36],
   [2, 'Beyza', 25]
 ]
+
+// Intersection Types
+type A = { name: string }
+type B = { age: number }
+type AB = A & B // must have BOTH name and age
+
+// Union
+let userId: number | string
+userId = '88'
+
+// Literal Types
+type Directions = 'up' | 'down' | 'left' | 'right'
+let move: Directions = 'up' // only 4 options above allowed
+
+// Enums
+enum Direction {
+  Up = 1, // Starts at 0 if left empty
+  Right, // Can be a string as well like Right = "right"
+  Down,
+  Left
+}
+// console.log(Direction.Left)
+
+// Objects
+type User = {
+  id: number
+  name: string
+}
+
+const typeUser1: User = {
+  id: 1,
+  name: 'Yasin'
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number // alternate way
+
+// Functions
+function concat(str1: string, str2: string): string {
+  // if there is no return the return type is void
+  return str1 + ' ' + str2
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number // read only
+  name: string
+  age?: number // optional
+}
+
+const interfaceUser1: UserInterface = {
+  id: 1,
+  name: 'Yasin'
+}
+
+interface ProUser extends UserInterface {
+  isAdmin: boolean
+}
+
+interface MathFunc {
+  (x: number, y: number): number
+}
+const add: MathFunc = (num1: number, num2: number): number => num1 + num2
+const subtract: MathFunc = (num1: number, num2: number): number => num1 - num2
+
+// Classes
+interface PersonInterface {
+  id: number
+  name: string
+  register(): string
+}
+
+class Person implements PersonInterface {
+  id: number // private = only visible in this class
+  name: string // protected = only visible in this class and its subclasses
+
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
+
+  register() {
+    return `${this.name} is now registered!`
+  }
+}
+
+const yasin = new Person(1, 'Yasin')
+
+// Subclasses
+class Employee extends Person {
+  position: string
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name)
+    this.position = position
+  }
+}
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3])
+let strArray = getArray<string>(['HTML', 'CSS', 'JS'])
+
+// Partial<T>: Makes all properties in T optional.
+// Required<T>: Makes all properties in T required.
+// Readonly<T>: Makes all properties in T read-only.
+// Pick<T, K>: Picks a set of properties K from T.
+// Omit<T, K>: Constructs a type by omitting keys K from T.
