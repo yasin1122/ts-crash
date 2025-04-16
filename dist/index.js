6522586fcbeed964370b6490eb208591f464409f
Yasin Cakal <whoisyasin@gmail.com>
2025-04-15 19:23:47 -0700

    1st

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,9 @@ let x = 'Hello';
 let ids = [1, 2, 3, 4, 5];
 // Tuple
 let person = [1, 'Yasin', true];
+// Tuple Array
+let people;
+people = [
+    [1, 'Yasin', 36],
+    [2, 'Beyza', 25]
+];
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,9 @@ let ids: number[] = [1, 2, 3, 4, 5]
 
 // Tuple
 let person: [number, string, boolean] = [1, 'Yasin', true]
+// Tuple Array
+let people: [number, string, number][]
+people = [
+  [1, 'Yasin', 36],
+  [2, 'Beyza', 25]
+]
