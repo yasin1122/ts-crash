18330fe18011863cf8db43b52feeddd976770276
Yasin Cakal <whoisyasin@gmail.com>
2025-04-15 19:21:26 -0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Basic Types
+let id = 8;
+let myName = 'Yasin';
+let isActive = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+// Tuple
+let person = [1, 'Yasin', true];
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Basic Types
+let id: number = 8
+let myName: string = 'Yasin'
+let isActive: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1, 2, 3, 4, 5]
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Yasin', true]
