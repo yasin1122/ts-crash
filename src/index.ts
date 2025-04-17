c319b60389521d23e6853333285138772baaa8a7
Yasin Cakal <whoisyasin@gmail.com>
2025-04-17 09:40:22 -0700

    comments

--- src/index.ts
+++ src/index.ts
@@ -123,6 +123,7 @@ function getArray<T>(items: T[]): T[] {
 let numArray = getArray<number>([1, 2, 3])
 let strArray = getArray<string>(['HTML', 'CSS', 'JS'])
 
+// Try implementing these
 // Partial<T>: Makes all properties in T optional.
 // Required<T>: Makes all properties in T required.
 // Readonly<T>: Makes all properties in T read-only.
