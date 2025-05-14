6481a2d1661a33d05376cbe9e63bd398484e31d4
Yasin Cakal <whoisyasin@gmail.com>
2025-05-13 22:13:11 -0700

    palindromes

--- dist/dsa.js
+++ dist/dsa.js
@@ -7,8 +7,19 @@ function reverse(str) {
   // return result
 
   // return str.split('').reverse().join('')
-
   return str.split('').reduce((reversed, char) => char + reversed, '')
 }
-
 console.log(reverse('hello'))
+
+// Section 4: Palindromes
+function palindrome(str) {
+  // return str === str.split('').reverse().join('')
+
+  // return str === str.split('').reduce((reversed, char) => char + reversed, '')
+
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) return false
+  }
+  return true
+}
+console.log(palindrome('abcdcba'))
